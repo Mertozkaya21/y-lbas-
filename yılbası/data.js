87dcaa47df07446453449a596500362f5de9e1e1
Mert Özkaya <mertozkaya2k1@gmail.com>
2025-12-02 18:39:41 +0300

    sssadsa

---
+++ yılbası/data.js
@@ -0,0 +1,65 @@
+window.SECRET_SANTA_USERS = [
+  {
+    username: "sercan",
+    password: "sercan123",
+    displayName: "Sercan",
+    receiver: {
+      name: "Seda",
+      hints: ""
+    }
+  },
+  {
+    username: "seda",
+    password: "sedoş",
+    displayName: "Seda",
+    receiver: {
+      name: "Mert",
+      hints: ""
+    }
+  },
+  {
+    username: "mert",
+    password: "mertosch",
+    displayName: "Mert",
+    receiver: {
+      name: "Ahmet",
+      hints: ""
+    }
+  },
+  {
+    username: "ahmet",
+    password: "alptekin",
+    displayName: "Ahmet",
+    receiver: {
+      name: "Ebru",
+      hints: ""
+    }
+  },
+  {
+    username: "ebru",
+    password: "delihasanoğlu",
+    displayName: "Ebru",
+    receiver: {
+      name: "Emine",
+      hints: ""
+    }
+  },
+  {
+    username: "emine",
+    password: "emineabla",
+    displayName: "Emine",
+    receiver: {
+      name: "Görkem",
+      hints: ""
+    }
+  },
+  {
+    username: "görkem",
+    password: "görkemgörkem",
+    displayName: "Görkem",
+    receiver: {
+      name: "Sercan",
+      hints: ""
+    }
+  }
+];
